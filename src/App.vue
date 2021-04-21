7f19996183555971be8346df4027977101aec168
Seba1997-abdo <abderrzakaliseba@gmail.com>
2021-04-21 16:33:01 +0100

    hak jdid

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld /> 
+  <Seba/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Seba from './components/Seba.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Seba  
   }
 }
 </script>
---
+++ src/components/Seba.vue
@@ -0,0 +1,53 @@
+<template>
+    
+  <div>
+   
+<h1> {{message}}</h1>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:"seba",
+  data(){
+
+     return{
+        message:"SEBA bienvenue"
+     }
+
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
