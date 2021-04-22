2b29675b17976449bb48faa03ffd08fd3816a831
Seba1997-abdo <abderrzakaliseba@gmail.com>
2021-04-22 14:38:11 +0100

    zid hadi

--- src/components/Seba.vue
+++ src/components/Seba.vue
@@ -6,17 +6,25 @@
 <h1> {{message2}}</h1>
  <button @click="changeMsg">changer le </button>    <!-- --> 
   </div>
+  <div class="posts" v-for="(post,id) in posts" :key="id">
+  <h2>{{post.title}}</h2>
+   <p>{{post.body}}</p>
+   
+  </div>
 
-</template>
+</template> 
 
 <script>
+import axios from '../../node_modules/axios'
+//import axios1 from 'axios'
 export default {
   name:"seba",
   data(){
 
      return{ 
         message:"SEBA bienvenue",
-        message2:"Hello"
+        message2:"Hello",
+        posts:[]
      }
 
   },
@@ -27,10 +35,18 @@ export default {
   },
   created(){
 
-      alert("created")
-
+     // alert("created")
+      axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(x=>{
+       // console.log(x.data);
+        this.posts=x.data
+      }).catch(e=>{
+           console.log(e+"aaaaaezaeaeaze")
+          // document.write(e+"azerr")
+           
+      })
   },
-  beforeCreate(){
+  beforeCreate(){   
        
        alert("before created")  
   },
